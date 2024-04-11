67daeb9897a076289869c33c5993c2d401d7ee47
joseph.spearhead@gmail.com <ofodumjoseph@yahoo.com>
2024-04-11 17:14:33 +0100

    implicit animation on the favorites icon

--- lib/screens/meal_details.dart
+++ lib/screens/meal_details.dart
@@ -21,20 +21,30 @@ class MealDetailsScreen extends ConsumerWidget {
     return Scaffold(
         appBar: AppBar(title: Text(meal.title), actions: [
           IconButton(
-            onPressed: () {
-              final wasAdded = ref
-                  .read(favoriteMealsProvider.notifier)
-                  .toggleMealFavoriteStatus(meal);
-              ScaffoldMessenger.of(context).clearSnackBars();
-              ScaffoldMessenger.of(context).showSnackBar(
-                SnackBar(
-                  content: Text(
-                      wasAdded ? 'Meal added as a favorite.' : 'Meal removed.'),
+              onPressed: () {
+                final wasAdded = ref
+                    .read(favoriteMealsProvider.notifier)
+                    .toggleMealFavoriteStatus(meal);
+                ScaffoldMessenger.of(context).clearSnackBars();
+                ScaffoldMessenger.of(context).showSnackBar(
+                  SnackBar(
+                    content: Text(wasAdded
+                        ? 'Meal added as a favorite.'
+                        : 'Meal removed.'),
+                  ),
+                );
+              },
+              icon: AnimatedSwitcher(
+                duration: const Duration(milliseconds: 300),
+                transitionBuilder: ((child, animation) => RotationTransition(
+                      turns: Tween<double>(begin: 0.5, end: 1).animate(animation ),
+                      child: child,
+                    )),
+                child: Icon(
+                  isFavorite ? Icons.star : Icons.star_border,
+                  key: ValueKey(isFavorite),
                 ),
-              );
-            },
-            icon: Icon(isFavorite ? Icons.star : Icons.star_border),
-          )
+              ))
         ]),
         body: SingleChildScrollView(
           child: Column(
